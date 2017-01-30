ba2dde24f251b317698b9ab5ee27c1d77440fa3a
Kamil Kuklinski <varinator@gmail.com>
2017-01-30 09:47:29 +0000

    Add files via upload

---
+++ jquery.equalheight.js
@@ -0,0 +1,41 @@
+/*
+
+ Version: 0.1
+  Author: Varin aka Kamil Kuklinski
+ Website:
+    Docs:
+    Repo:
+  Issues:
+
+ */
+
+ (function($){
+
+  	$.fn.extend({
+
+  		equalHeight: function() {
+        var item = this;
+
+     		return this.each(function() {
+
+        var maxHeight = 0;
+          $(item).each(function () {
+
+            var singleHeight = $(this).outerHeight();
+
+            if  (singleHeight > maxHeight) {
+              maxHeight = singleHeight;
+            }
+
+          });
+
+        console.log(maxHeight);
+         $(item).parent().css('min-height', maxHeight);
+
+      //  $(item).parent().css('min-height', (maxHeight + ($(item).parent().outerHeight()) - ($(item).parent().height())) );
+
+     		});
+
+     	}
+ 	});
+ })(jQuery);
